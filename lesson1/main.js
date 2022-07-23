ef0c6b8c69e6fb9ac1b719ed4ae5d88d8052476a
OleksandrShevchenko <shevchenko2106@gmail.com>
2022-07-23 12:24:44 +0300

    Initial commit

---
+++ lesson1/main.js
@@ -0,0 +1,97 @@
+let str1 = "hello";
+let str2 = "owu";
+let str3 = "com";
+let str4 = "ua";
+
+let n1 = 1;
+let n2 = 10;
+let n3 = -999;
+let n4 = 123;
+let n5 = 3.14;
+let n6 = 2.7;
+let n7 = 16;
+
+let bool1 = true;
+let bool2 = false;
+
+console.log(str1);
+console.log(str2);
+console.log(str3);
+console.log(str4);
+
+console.log(n1);
+console.log(n2);
+console.log(n3);
+console.log(n4);
+console.log(n5);
+console.log(n6);
+console.log(n7);
+
+console.log(bool1);
+console.log(bool2);
+
+alert(str1);
+alert(str2);
+alert(str3);
+alert(str4);
+
+alert(n1);
+alert(n2);
+alert(n3);
+alert(n4);
+alert(n5);
+alert(n6);
+alert(n7);
+
+alert(bool1);
+alert(bool2);
+
+document.write(str1);
+document.write(str2);
+document.write(str3);
+document.write(str4);
+
+document.write(n1);
+document.write(n2);
+document.write(n3);
+document.write(n4);
+document.write(n5);
+document.write(n6);
+document.write(n7);
+
+document.write(bool1);
+document.write(bool2);
+
+// --------------------------------------------
+// --------------------------------------------
+// --------------------------------------------
+
+let firstName = 'Oleksandr';
+let middleName = 'Sergiyovuch';
+let lastName = 'Shevchenko';
+let space = ' ';
+let person = firstName + space + middleName + space + lastName;
+console.log(person);
+
+//-------------------------------------
+// --------------------------------------------
+// --------------------------------------------
+let a = 100;
+let b = '100';
+let c = true;
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+
+//-------------------------------------
+// --------------------------------------------
+// --------------------------------------------
+let name = prompt();
+let poBatkovi = prompt();
+let years = prompt();
+
+console.log(name);
+console.log(poBatkovi);
+console.log(years);
+
+
